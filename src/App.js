dcea375774e7931b9567056d6d62d2cad37e0c21
Victor <vbaziuk13@gmail.com>
2021-12-10 21:49:42 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(true)
 
   const [ tasks, setTasks] = useState([
     {
@@ -26,10 +29,37 @@ function App() {
 
 ])
 
+// placing functions in this 'root' file allows us to pass them down into the child components
+
+
+// add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+// in this example... [App.js] -> [Tasks.js] -> [Task.js]
+const deleteTask = (id) => {
+  // setting the tasks to the filtered tasks to perform delete (filter out all the ones that dont equal the passed in id)
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      { showAddTask && <AddTask onAdd={addTask} />}
+
+
+      {/* deleteTask gets passed down here to Tasks.js */}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show') }
+
+      
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
+import Task from "./Task"
 
-
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
 
     return (
         <>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )

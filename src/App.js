56fd64ac7fccdb073cd3c79146c97de7bf22f7ce
Victor <vbaziuk13@gmail.com>
2021-12-12 12:19:00 -0500

    commit backend progress

--- src/App.js
+++ src/App.js
@@ -1,66 +1,105 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
 
-  const [showAddTask, setShowAddTask] = useState(true)
+  // add task
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task),
+    })
 
-  const [ tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
+    const data = await res.json();
+
+    setTasks([...tasks, data])
+  }
+
+  /*
+   * placing functions in this 'root' file allows us to pass them down into the child components
+   * in this example... [App.js] -> [Tasks.js] -> [Task.js] 
+   */
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
+    // setting the tasks to the filtered tasks to perform delete (filter out all the ones that don't equal the passed in id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+
+  // fetch tasks
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
     }
 
-])
+    getTasks()
+  }, [])  // dependency array - when any value within the array changes, then this wil cause the useEffect to run!
 
-// placing functions in this 'root' file allows us to pass them down into the child components
+
+  // fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json()
+    return data
+  }
+
+  // fetch task by id
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json()
+    return data
+  }
 
 
-// add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask])
-}
+  // toggle reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-// in this example... [App.js] -> [Tasks.js] -> [Task.js]
-const deleteTask = (id) => {
-  // setting the tasks to the filtered tasks to perform delete (filter out all the ones that dont equal the passed in id)
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
 
-// toggle reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task))
-}
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
+  }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      { showAddTask && <AddTask onAdd={addTask} />}
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        {showAddTask && <AddTask onAdd={addTask} />}
 
 
-      {/* deleteTask gets passed down here to Tasks.js */}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show') }
+        {/* deleteTask gets passed down here to Tasks.js */}
+        {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
 
-      
-    </div>
+        <Routes>
+          <Route path='/about' element={<About/>} />
+        </Routes>
+        <Footer />
+      </div>
+
+    </Router>
   );
 }
 
